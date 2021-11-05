9d41ee759002fb6348ff260afc19ada417dd6fa0
jpenabolivar <jiseth.pena@segurosbolivar.com>
2021-11-04 22:42:10 -0500

    Linear search in a skip list

---
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -0,0 +1,44 @@
+#include "search.h"
+/**
+ * linear_skip - that searches for a value in a sorted skip list of integers.
+ * @list: is a pointer to the head of the skip list to search.
+ * @value:  is the value to search for.
+ *
+ * Return: a pointer where the value was found.
+ */
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+	skiplist_t *node;
+
+	if (!list)
+		return (NULL);
+
+	node = list->express;
+
+	printf("Value checked at index [%lu] = [%d]\n", node->index, node->n);
+    
+	while (node && node->n < value)
+	{
+		list = node;
+		node = node->express;
+		if (!node)
+		{
+			node = list;
+			while (node->next)
+				node = node->next;
+			break;
+		}
+		printf("Value checked at index [%lu] = [%d]\n",	node->index, node->n);
+	}
+	printf("Value found between indexes [%lu] and [%lu]\n", list->index,
+			node->index);
+
+	for (; list && node->n >= list->n; list = list->next)
+	{
+		printf("Value checked at index [%lu] = [%d]\n", list->index, list->n);
+
+		if (list->n == value)
+			return (list);
+	}
+	return (NULL);
+}
